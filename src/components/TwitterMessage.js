f8e05b35154f6e5cb47e628f4bdb5ebaf08e3d5d
yatsun <yatsunlam25@gmail.com>
2021-11-04 22:35:41 -0400

    commit tree 00dfafc0cb69855783a2c4fdfb0377190861c42e

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {message: ""};
+  }
+
+  handleClick = (e) => {
+    if (e.target.value.length <= this.props.maxChars) {
+      this.setState({message: e.target.value})
+    } else {
+      alert("Character limit")
+    }
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+        onChange={this.handleClick}
+        value={this.state.message}
+        />
+        <em>Characters left: {this.props.maxChars - this.state.message.length}</em>
       </div>
     );
   }
